0365f3c0ea83fde13d9577115b3190260f254a06
MichelleZhao2022 <michelle.zhao@dovetailstudios.com>
2022-12-14 12:10:18 +1300

    add vue3-carousel package for list a bunch of images in accommodation detail page

--- app/javascript/views/api/accommodation/show.vue
+++ app/javascript/views/api/accommodation/show.vue
@@ -3,13 +3,33 @@
     <span>{{ accommodation.city }}</span><br />
     <span>{{ accommodation.country }}</span><br />
     <span>in detail page</span><br />
+
+    <carousel :items-to-show="1.5">
+    <slide v-for="slide in 10" :key="slide">
+      {{ slide }}
+    </slide>
+
+    <template #addons>
+      <navigation />
+      <pagination />
+    </template>
+  </carousel>
   </div>
 </template>
 <script>
 import axios from "axios";
 import { reactive, toRefs } from "vue";
 import { useRoute } from "vue-router";
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
+
 export default {
+  components: {
+    Carousel,
+    Slide,
+    Pagination,
+    Navigation,
+  },
   setup() {
     const route = useRoute();
     const state = reactive({ accommodation: {} });
